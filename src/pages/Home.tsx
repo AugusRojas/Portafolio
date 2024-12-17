be1ac70b741672df82a0e45fb485aebe54742a5e
AugusRojas <augus32132@gmail.com>
2024-12-17 14:19:53 -0300

    Subido

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import About from "../components/About"
 import Projects from "../components/Projects"
 import Contact from "../components/Contact"
-import '../App.css'
 
 const Home = () => {
   return (
