a7213ff9e3adee1f8ee99395d5a86bc73be0ff23
AugusRojas <augus32132@gmail.com>
2024-12-17 14:06:40 -0300

    Subido

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Home from './pages/Home'
 
+
 function App() {
   return (
     <div 
       style={{ 
-        background: 'linear-gradient(135deg, #ff7e5f, #feb47b)', 
-        minHeight: '100vh', 
-        margin: 0 
+        // background: 'linear-gradient(135deg, #ff7e5f, #feb47b)', 
+        // minHeight: '100vh', 
+        // margin: 0 
       }}
     >
       <Home/>  
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import About from "../components/About"
 import Projects from "../components/Projects"
 import Contact from "../components/Contact"
+import './App.css'
 
 const Home = () => {
   return (
