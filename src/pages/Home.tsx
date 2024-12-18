d34c11d0db82b621ace2c47f2cd072c84b4439cc
AugusRojas <augus32132@gmail.com>
2024-12-18 15:25:24 -0300

    Subido

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,40 @@
 import About from "../components/About"
 import Projects from "../components/Projects"
 import Contact from "../components/Contact"
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 const Home = () => {
   return (
   <div className="">
-    <nav>
+  <Router>
+  <nav>
     <div className="nav-wrapper" style={{ 
         backgroundColor: '#4CAF50', 
         color: 'white', 
         padding: '20px' 
       }}>
       <ul id="nav-mobile" className="right hide-on-med-and-down color ">
-        <li><a>Sobre mí</a></li>
-        <li><a>Proyectos</a></li>
-        <li><a>Contacto</a></li>
+        <li><Link to="/About">Sobre mí</Link></li>
+        <li><Link to="/Projects">Proyectos</Link></li>
+        <li><Link to="/Contact">Contactos</Link></li>
       </ul>
+
+      <Routes>
+          <Route path="/About" element={<About />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Contact" element={<Contact />} />
+      </Routes>
+
+
     </div>
   </nav>
+  </Router>
+
   <br />
   <About/>
   <Projects/>
   <Contact/>
-    </div>
+  </div>
   )
 }
 
